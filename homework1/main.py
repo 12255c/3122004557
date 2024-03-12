256206223c882e0aff912b8adfc4a83293eb59f1
cyp <985161400@qq.com>
2024-03-12 21:45:21 +0800

    finish remove part

--- homework1/main.py
+++ homework1/main.py
@@ -10,5 +10,23 @@ def read_file(path):
             return file.read()
     except FileNotFoundError:
         print(f'文件不存在：{path}')
-        return FileNotFoundError
-    
+    except Exception as e:
+        print(f'读取文件时发生错误：{e}')
+    return None
+
+
+def remove_punctuation_and_lowercase(text):
+    # 去除文本当中的标点符号并将文本小写
+    try:
+        text = text.translate(str.maketrans('', '', string.punctuation)).lower().replace(' ', '')
+        return text
+    except Exception as e:
+        print(f'处理文本时发生错误：{e}')
+        return None
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
