f1dd036ab37bc02b5e9826f7c7f05cce4dd55591
cyp <985161400@qq.com>
2024-03-12 21:21:23 +0800

    file reading is completed

--- homework1/main.py
+++ homework1/main.py
@@ -1,1 +1,14 @@
-1
+import difflib
+import string
+import sys
+
+
+def read_file(path):
+    # 读取文件
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f'文件不存在：{path}')
+        return FileNotFoundError
+    
